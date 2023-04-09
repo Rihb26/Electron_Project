0139d19d936b25e6e777472643892ed3264f80ee
RihabABDELLI <118831638+Rihb26@users.noreply.github.com>
2023-04-09 13:47:46 +0200

    Update musicapp.js

--- src/screens/App/musicapp.js
+++ src/screens/App/musicapp.js
@@ -1,9 +1,53 @@
 import logo from './src/music.png';
-import './App.css';
 import DialogButton from './components/dialogButton/index.js.js';
 import React, { useState,useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
+const Container = styled.div`
+  background-color: wite;
+  text-align: center;
+`;
 
+const Logo = styled.img`
+  height: 30vmin;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spin} infinite 20s linear;
+  }
+`;
+
+const Header = styled.header`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 200;
+  margin-bottom: 20px;
+  color: white;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
 function Musicapp() { 
     const [filePath, setFile] = useState()
     
